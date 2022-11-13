7798ce780b99d9d818412cfe4681cf90c6ed3f30
Daniel <danielmosqueda15@gmail.com>
2022-11-13 03:10:05 -0500

    Added Background Title cards, and grids for movies

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -4,4 +4,4 @@ export const USER_SERVER = '/api/users';
 export const API_KEY = '1b4def07b2ce4a9a39fb3a19533b320b';
 export const API_URL ='https://api.themoviedb.org/3/'
 
-export const IMAGE_URL = 'http://image.tmbd.org/t/p/'
+export const IMAGE_URL = 'https://image.tmdb.org/t/p/'
